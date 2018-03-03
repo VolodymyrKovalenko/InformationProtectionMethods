a57b9947d04f2959ff9d18e40119d69f8321e24a
volodymyr <whitelol888@gmail.com>
2018-03-03 23:02:11 +0200

    add 404 error

--- CessarСhifr.py
+++ CessarСhifr.py
@@ -78,6 +78,10 @@ def TritemiusPost():
 
     return render_template('TritemiusPage.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
 
 class EncryptMessage():
     def __init__(self, message, key):
