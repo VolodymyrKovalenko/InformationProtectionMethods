104fff7ec5aa0169d0958caf6a665887faa9e943
volodymyr <whitelol888@gmail.com>
2018-04-02 18:35:51 +0300

    changes in caesar request

--- GamblingModule.py
+++ GamblingModule.py
@@ -7,6 +7,6 @@ class GamblingChifr(TritemiusChifr):
     def find_gambling_key(self):
         num_index = 1
         while num_index <= len(self.message):
-            self.key = (2*self.key-5) % (len(self.alphabet)-1)
+            self.key = (105*self.key-51) % (len(self.alphabet)-1)
             self.arr_of_keys.append(self.key)
             num_index += 1
--- app.py
+++ app.py
@@ -10,100 +10,70 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def firstPost():
+def caesar_app():
     if request.method == 'POST':
-        if 'form1' in request.form:
-            encr_text = request.form['TextEncr']
-            input_key = int(request.form['InpKey1'])
-            obj = CaesarChifr(encr_text, input_key)
-            obj.CreateAlphabet()
+        inp_text = request.form['InpText']
+        input_key = int(request.form['InpKey'])
+        obj = CaesarChifr(inp_text, input_key)
+        obj.CreateAlphabet()
+        if 'encr_form' in request.form:
             res_message = obj.encr()
-            return render_template('MainPage.html', my_data1=res_message)
-        elif 'form2' in request.form:
-            decr_text = request.form['TextDecr']
-            input_key = int(request.form['InpKey2'])
-            obj = CaesarChifr(decr_text, input_key)
-            obj.CreateAlphabet()
+            return render_template('MainPage.html', data_encr=res_message)
+        elif 'decr_form' in request.form:
             res_message = obj.decr()
-            return render_template('MainPage.html', my_data2=res_message)
+            return render_template('MainPage.html', data_decr=res_message)
 
     return render_template('MainPage.html')
 
 
 @app.route('/tritemius', methods=['GET', 'POST'])
-def TritemiusPost():
-    eror = 'Enter all the fields'
+def tritemius_app():
+    error = 'Enter all the fields'
     if request.method == 'POST':
         input_array = []
-        if 'form1' in request.form:
-            encr_text = request.form['TextEncr']
-            if len(request.form['InpKey1']) != 0:
-                input_array.append(request.form['InpKey1'])
+        input_text = request.form['InpText']
+        if len(request.form['InpKey']) != 0:
+            input_array.append(request.form['InpKey'])
 
-            elif len(request.form['InpKey21']) != 0:
-                if len(request.form['InpKey22']) != 0:
-                    input_array.extend((int(request.form['InpKey21']), int(request.form['InpKey22'])))
-                else:
-                    return render_template('TritemiusPage.html', er_info=eror)
-
+        elif len(request.form['InpKey1']) != 0:
+            if len(request.form['InpKey2']) != 0:
+                input_array.extend((int(request.form['InpKey1']), int(request.form['InpKey2'])))
             else:
-                if len(request.form['InpKey31']) != 0 and len(request.form['InpKey32']) != 0 and len(
-                        request.form['InpKey33']) != 0:
-                    input_array.extend(
-                        (int(request.form['InpKey31']), int(request.form['InpKey32']), int(request.form['InpKey33'])))
-                else:
-                    return render_template('TritemiusPage.html', er_info=eror)
+                return render_template('TritemiusPage.html', er_info=error)
 
-            obj = TritemiusChifr(encr_text, input_array)
-            obj.find_key()
-            obj.CreateAlphabet()
+        else:
+            if len(request.form['InpKey31']) != 0 and len(request.form['InpKey32']) != 0 and len(
+                    request.form['InpKey33']) != 0:
+                input_array.extend(
+                    (int(request.form['InpKey31']), int(request.form['InpKey32']), int(request.form['InpKey33'])))
+            else:
+                return render_template('TritemiusPage.html', er_info=error)
+
+        obj = TritemiusChifr(input_text, input_array)
+        obj.find_key()
+        obj.CreateAlphabet()
+        if 'encr_form' in request.form:
             res_message = obj.encr()
-            return render_template('TritemiusPage.html', my_data1=res_message)
-        elif 'form2' in request.form:
-            decr_text = request.form['TextDecr']
-            if len(request.form['InpKey4']) != 0:
-                input_array.append(request.form['InpKey4'])
-
-            elif len(request.form['InpKey51']) != 0:
-                if len(request.form['InpKey52']) != 0:
-                    input_array.extend((int(request.form['InpKey51']), int(request.form['InpKey52'])))
-                else:
-                    return render_template('TritemiusPage.html', er_info2=eror)
-
-            else:
-                if len(request.form['InpKey61']) != 0 and len(request.form['InpKey62']) != 0 and len(
-                        request.form['InpKey63']) != 0:
-                    input_array.extend(
-                        (int(request.form['InpKey61']), int(request.form['InpKey62']), int(request.form['InpKey63'])))
-                else:
-                    return render_template('TritemiusPage.html', er_info2=eror)
-
-            obj = TritemiusChifr(decr_text, input_array)
-            obj.find_key()
-            obj.CreateAlphabet()
+            return render_template('TritemiusPage.html', data_encr=res_message)
+        elif 'decr_form' in request.form:
             res_message = obj.decr()
-            return render_template('TritemiusPage.html', my_data2=res_message)
+            return render_template('TritemiusPage.html', data_decr=res_message)
 
     return render_template('TritemiusPage.html')
 
 
 @app.route('/gambling', methods=['GET', 'POST'])
-def gambling():
+def gambling_app():
     if request.method == 'POST':
-        if 'form1' in request.form:
-            encr_text = request.form['TextEncr']
-            input_key = int(request.form['InpKey1'])
-            obj = GamblingChifr(encr_text, input_key)
-            obj.CreateAlphabet()
-            obj.find_gambling_key()
+        input_text = request.form['InpText']
+        input_key = int(request.form['InpKey'])
+        obj = GamblingChifr(input_text, input_key)
+        obj.CreateAlphabet()
+        obj.find_gambling_key()
+        if 'encr_form' in request.form:
             res_message = obj.encr()
             return render_template('Gambling.html', data_encr=res_message)
-        elif 'form2' in request.form:
-            decr_text = request.form['TextDecr']
-            input_key = int(request.form['InpKey2'])
-            obj = GamblingChifr(decr_text, input_key)
-            obj.CreateAlphabet()
-            obj.find_gambling_key()
+        elif 'decr_form' in request.form:
             res_message = obj.decr()
             return render_template('Gambling.html', data_decr=res_message)
     return render_template('Gambling.html')
